7983c9defa1a3f47c328e2c7e52ca13e7d2c5814
unknown <steven.eiselen@gmail.com>
2015-09-23 10:56:33 -0700

    updated GUI manager

--- JukeBoxStart/src/demoSongPlayer/Play1SongNoListener.java
+++ JukeBoxStart/src/demoSongPlayer/Play1SongNoListener.java
@@ -1,19 +1,10 @@
 package demoSongPlayer;
 
-/**
- * Play one audio file from the songfiles directory.
- * There is no listener for the end of song event.
- * 
- * @author Rick Mercer
- */
+
 import songplayer.SongPlayer;
 
 public class Play1SongNoListener {
 
-  /**
-   * Play one audio file with no listener for the end of song event.
-   * A println is included to indicate the song is playing in a separate Thread.
-   */
   public static void main(String[] args) {
     SongPlayer.playFile("./songfiles/BlueRidgeMountainMist.mp3");
 
