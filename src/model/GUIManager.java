c8d0a37fb6c1cd8ca585122e1bb8124991853de0
unknown <Steven@UITS-STEEIS.catnet.arizona.edu>
2015-09-26 19:26:13 -0700

    RE-MADE GUI MANAGER

--- src/model/GUIManager.java
+++ src/model/GUIManager.java
@@ -6,7 +6,9 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.Map.Entry;
@@ -21,37 +23,109 @@ public class GUIManager extends JFrame {
 			
 	private JLabel headerMessage = new JLabel("Select a Song from this Jukebox!!!");
 	private JTable myTable;
+	private SongDisplayList mySongList;
+	private JScrollPane myScrollPane;
 	
 	public GUIManager(SongCollection allSongs){	
-		setUpTable(allSongs);
-		setUpWindow();
+		setUpGUI(allSongs);
 	} // Ends Constructor
-	
-	private void setUpTable(SongCollection songs){
-		String[] colNames = {"Artist" , "Title" , "Seconds"};
-		int listSize = songs.getSize();
-		HashMap<String, Song> yolo = songs.songs;
-		Set<Entry<String, Song>> mySet = yolo.entrySet();
-		String info[][] = new String[listSize][3]; 
 		
-		int i = 0;
-		for ( Entry<String, Song> e : mySet){
-			Song s = e.getValue();
-			info[i][0] = s.getArtist();
-			info[i][1] = s.getTitle();
-			info[i][2] = "" + s.getDurationOfSong();
-			i++;
-		}
-		myTable = new JTable(info, colNames);
-	} // Ends Method setUpTable
-	
-	private void setUpWindow(){
-		this.setSize(600, 600);
+	private void setUpGUI(SongCollection songs){
+		// Set Window Sizes
+		this.setSize(600, 540);
+		this.setLocation(120, 60);
+		// Set Closing Process
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Set up the Header
 		headerMessage.setForeground(Color.BLUE);
 		headerMessage.setFont(new Font("Arial", Font.BOLD, 18));
 		this.add(headerMessage, BorderLayout.NORTH);
-		this.add(new JScrollPane(myTable));
-	} // Ends Method setUpWindow
+		// Set up the model, the table, the scrollpane and add it!
+		mySongList = new SongDisplayList(songs);
+		myTable = new JTable(mySongList);
+		myScrollPane = new JScrollPane(myTable);
+		this.add(myScrollPane);
+	} // Ends Method setUpGUI
 	
-
 } // Ends Class GUIManager
+
+class SongDisplayList implements TableModel{
+	
+	private ArrayList<Song> allSongs = new ArrayList<Song>(); // holds the songList in ArrayList Form
+	
+	public SongDisplayList(SongCollection songs) {
+		setUpList(songs);
+	} // Ends Constructor
+	
+	private void setUpList(SongCollection songs){
+		HashMap<String, Song> yolo = songs.songs;
+		Set<Entry<String, Song>> mySet = yolo.entrySet();
+
+		int i = 0;
+		for ( Entry<String, Song> e : mySet){
+			allSongs.add(e.getValue());
+		}
+	} // Ends Method setUpTable
+	
+	@Override
+	public int getRowCount() {
+		return allSongs.size();
+	} // Ends Method getRowCount
+	
+	@Override
+	public int getColumnCount() {
+		return 3;
+	} // Ends Method getColumnCount
+	
+	@Override
+	public String getColumnName(int colNum) {
+		switch(colNum){
+		case 0:	return "Artist";
+		case 1: return "Title";
+		case 2: return "Seconds";
+		default: return "ERROR";
+		}
+	} // Ends Method getColumnName
+	
+	@Override
+	public Class<?> getColumnClass(int colNum) {
+		switch(colNum){
+		case 0:	return String.class;
+		case 1: return String.class;
+		case 2: return Integer.class;
+		default: return Object.class;
+		}
+	} // Ends Method getColumnClass
+	
+	@Override
+	public Object getValueAt(int r, int c) {
+		Song aSong = allSongs.get(r);
+		switch(c){
+		case 0:	return aSong.getArtist();
+		case 1: return aSong.getTitle();
+		case 2: return aSong.getDurationOfSong();
+		default: return "ERROR";
+		}
+	} // Ends Method getValueAt
+	
+	@Override
+	public void addTableModelListener(TableModelListener arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public boolean isCellEditable(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeTableModelListener(TableModelListener arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void setValueAt(Object arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+	}
+} // Ends Class SongDisplayList
--- src/model/SongCollection.java
+++ src/model/SongCollection.java
@@ -26,10 +26,8 @@ public class SongCollection {
 	} // Ends Method getSong
 		
 	private void populateSongs(){
-		songs.put("Are You Gonna Go My Way?/Lenny Kravitz", new Song("Are You Gonna Go My Way?", "Lenny Kravitz", "filename.mp3", 284));
 		songs.put("Satisfied/8stops7", new Song("Satisfied", "8stops7", "filename.mp3", 185));
 		songs.put("Stop the Rock (Mint Royale Mix)/Apollo 440", new Song("Stop the Rock (Mint Royale Mix)", "Apollo 440", "filename.mp3", 372));
-		songs.put("Mad Skillz � Mic Chekka/BT", new Song("Mad Skillz � Mic Chekka", "BT", "filename.mp3", 336));
 		songs.put("Break In/CiRRUS", new Song("Break In", "CiRRUS", "filename.mp3", 285));
 		songs.put("Sky Scraper/Daiki Kasho", new Song("Sky Scraper", "Daiki Kasho", "filename.mp3", 335));
 		songs.put("Go Gran Turismo/Dave Aude", new Song("Go Gran Turismo", "Dave Aude", "filename.mp3", 185));
@@ -40,13 +38,11 @@ public class SongCollection {
 		songs.put("Sober (Saint US Mix)/Muse", new Song("Sober (Saint US Mix)", "Muse", "filename.mp3", 248));
 		songs.put("Def Beat/Junkie XL", new Song("Def Beat", "Junkie XL", "filename.mp3", 208));
 		songs.put("Crash/Methods of Mayhem", new Song("Crash", "Methods of Mayhem", "filename.mp3", 202));
-		songs.put("Kickstart My Heart/M�tley Cr�e", new Song("Kickstart My Heart", "M�tley Cr�e", "filename.mp3", 288));
 		songs.put("Never Enough/Papa Roach", new Song("Never Enough", "Papa Roach", "filename.mp3", 214));
 		songs.put("Super Nova Goes Pop/Powerman 5000", new Song("Super Nova Goes Pop", "Powerman 5000", "filename.mp3", 213));
 		songs.put("Determination/Raekwon", new Song("Determination", "Raekwon", "filename.mp3", 192));
 		songs.put("Dogg's Turismo 3/Snoop Dogg", new Song("Dogg's Turismo 3", "Snoop Dogg", "filename.mp3", 289));
 		songs.put("She Sells Sanctuary/The Cult", new Song("She Sells Sanctuary", "The Cult", "filename.mp3", 263));
-		songs.put("Againa/Lenny Kravitz", new Song("Againa", "Lenny Kravitz", "filename.mp3", 225));
 		songs.put("Just a Day (Alan Moulder Mix)/Feeder", new Song("Just a Day (Alan Moulder Mix)", "Feeder", "filename.mp3", 233));
 		songs.put("Shark/Ash", new Song("Shark", "Ash", "filename.mp3", 199));
 		songs.put("Stompbox/Overseer", new Song("Stompbox", "Overseer", "filename.mp3", 237));
